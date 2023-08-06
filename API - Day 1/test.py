e8ebf0b96f1c2197e73aa46671e0524e56576d6a
udayavel <36785195+udayavel@users.noreply.github.com>
2023-08-07 01:15:18 +0530

    Add files via upload

---
+++ API - Day 1/test.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/abc',methods=['GET','POST'])
+def test1():
+    if(request.method=='POST'):
+        a=request.json['num1']
+        b=request.json['num2']
+        result = a+b
+        return jsonify(str(result))
+
+@app.route('/abc1',methods=['GET','POST'])
+def test2():
+    if (request.method == 'POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a * b
+        return jsonify(str(result))
+
+
+if __name__=='__main__':
+    app.run()
+
+# def test(a,b):
+#     return a+b
+
+# print(test(1,2))
